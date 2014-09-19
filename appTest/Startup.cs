1bc0200163f3ad4a7588021be7bf3c4cf19f2d9d
Alexsandro Frozza <frozza89@gmail.com>
2014-09-18 20:59:21 -0400

    projeto inicial e gitignore

---
+++ appTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(appTest.Startup))]
+namespace appTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
